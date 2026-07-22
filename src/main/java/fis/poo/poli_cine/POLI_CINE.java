06d385788e3a0829d8d19c614df7ff5d2f15e3f2
sebas <sebas@penguin>
2026-07-22 12:07:22 -0500

    Primer commit

---
+++ src/main/java/fis/poo/poli_cine/POLI_CINE.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fis.poo.poli_cine;
+
+/**
+ *
+ * @author sebas
+ */
+public class POLI_CINE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
